308dc8b06d7aea75751dfe2469d048bf6b39faec
木 <98724914+kireikireina@users.noreply.github.com>
2025-04-09 14:43:20 +0700

    Add files via upload

---
+++ Testing/Db2/Db2ConnectApplication.java
@@ -0,0 +1,13 @@
+package net.code.java.Db2Connect;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Db2ConnectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Db2ConnectApplication.class, args);
+	}
+
+}
